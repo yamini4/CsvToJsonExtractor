21c6e6e270b0c6239be59e51405564091e8009cd
yamini.g <yamini.g@indusind.com>
2025-04-22 15:19:51 +0530

    changes

--- src/main/java/com/indusind/utility/Utility.java
+++ src/main/java/com/indusind/utility/Utility.java
@@ -7,8 +7,6 @@ import java.util.Optional;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.annotation.PostConstruct;
-
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.DateUtils;
 import org.slf4j.Logger;
@@ -20,44 +18,26 @@ import org.springframework.stereotype.Service;
 public class Utility {
 
 	private String outputPattern;
-	private String dateFormatsRegex;
-//	private String dateFormats;
+//	private String dateFormatsRegex;
 	private String[] requestDateFormat;
 	private Pattern DATE_PATTERN;
 
-	@Value("${output-date-pattern}")
-	private String outputPatternProp;
-
-	@Value("${date-formats-regex}")
-	private String dateFormatsRegexProp;
-
-	@Value("${date-formats}")
-	private String dateFormatsProp;
+//	@Value("${output-date-pattern}")
+//	private String outputPatternProp;
+//
+//	@Value("${date-formats-regex}")
+//	private String dateFormatsRegexProp;
+//
+//	@Value("${date-formats}")
+//	private String dateFormatsProp;
 
 	public Utility(@Value("${output-date-pattern}") String ouputPattern,
-			@Value("${date-formats-regex}") String dateFormatsRegex,
-			@Value("${date-formats}") String dateFormats) {
+			@Value("${date-formats-regex}") String dateFormatsRegex, @Value("${date-formats}") String dateFormats) {
 		this.requestDateFormat = dateFormats.split(",");
 		this.outputPattern = ouputPattern;
-		this.dateFormatsRegex = dateFormatsRegex;
+//		this.dateFormatsRegex = dateFormatsRegex;
 		DATE_PATTERN = Pattern.compile(dateFormatsRegex);
 	}
-
-//	@PostConstruct
-//	public void init() {
-//		try {
-//			this.outputPattern = outputPatternProp;
-//			this.dateFormatsRegex = "\\b(0?[1-9]|[12][0-9]|3[01])/(0?[1-9]|1[0-2])/\\d{4}\\b|\\b\\d{4}-\\d{2}-\\d{2}(?:T\\d{2}:\\d{2}:\\d{2}(?:\\.\\d{3,9})?(?:Z)?)?\\b|\\b\\d{4}/\\d{2}/\\d{2}\\b|\\b\\d{2}/\\d{2}/\\d{4}\\b|\\b\\d{2}-\\d{2}-\\d{4}\\b|\\b\\d{4}/\\d{2}/\\d{2} \\d{2}:\\d{2}:\\d{2}\\b|\\b\\d{2}/\\d{2}/\\d{4} \\d{2}:\\d{2}:\\d{2}\\b|\\b\\d{4}-\\d{2}-\\d{2}T\\d{2}:\\d{2}:\\d{2}\\b";
-//			this.dateFormats = dateFormatsProp;
-//			this.requestDateFormat = dateFormats.split(",");
-//			System.out.println(dateFormatsRegex);
-//			this.DATE_PATTERN = Pattern.compile(dateFormatsRegex);
-//			System.out.println(DATE_PATTERN);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//
-//	}
 
 	private static final Logger logger = LoggerFactory.getLogger(Utility.class);
 
